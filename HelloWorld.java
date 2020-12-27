497101cac5fa8886f29b7f421302d786598a41b5
Rados95 <radosacimovic@gmail.com>
2020-12-27 15:00:52 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Welcome to Jenkins from Github!");
+		System.out.println("Hello to Jenkins from Github!");
 	}
 }
